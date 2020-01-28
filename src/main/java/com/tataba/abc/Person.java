75a8212dc06cbd4f4a01ab2f7560cf2c9ec2e581
wangyq <wangyq@onbonbx.com>
2020-01-28 17:07:37 +0800

    添加初始化工程

---
+++ src/main/java/com/tataba/abc/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.tataba.abc;
+
+public class Person {
+    
+    public String name;
+    public int age;
+    
+    public Person (String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Name: " + this.name + " age: " + this.age;
+    }
+}
